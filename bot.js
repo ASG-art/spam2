fb75721f704adf12d1626d783656455d013a4c64
ASG-art <54358751+ASG-art@users.noreply.github.com>
2019-08-21 18:54:10 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const client2 = new Discord.Client();
+const client3 = new Discord.Client();
 
 client.on('ready', () => {
    console.log(`----------------`);
@@ -80,9 +81,22 @@ if (message.content === '!spam') {
         }
       }
 });
-
+client.on('message', message => { // لا تغير شئ عشان ما تخرب الدنيا
+if (message.content === '!spam') {
+	let count = 0;
+	let ecount = 0;
+	for(let x = 0; x < 90000; x++) {
+		message.channel.send(`**Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج **[ " ${x} " ]`)
+		.then(m => {
+			count++;
+		})
+					
+        }		
+      }	
+});		
 
 
 
 client.login(process.env.TOKEN);// لا تغير فيها شيء
 client2.login(process.env.TOKEN2);// لا تغير فيها شيء
+client2.login(process.env.TOKEN3);// لا تغير فيها شيء
